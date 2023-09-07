45d4038e8bd40ccabacfe6623889fc3770b188f8
David--Cléris Timothée <timothee.davidcleri@gmail.com>
2023-09-07 15:27:26 +0000

    add export image

---
+++ export_images.sh
@@ -0,0 +1,3 @@
+docker image save sycldocker-ubuntu20.04 -o sycldocker-ubuntu20.04.tar.gz
+docker image save sycldocker-ubuntu20.04_llvm15_cuda11.0.2_rocm5.4.3 -o sycldocker-ubuntu20.04_llvm15_cuda11.0.2_rocm5.4.3.tar.gz
+docker image save sycldocker-sycl_ubuntu20.04_llvm15_cuda11.0.2_rocm5.4.3 -o sycldocker-sycl_ubuntu20.04_llvm15_cuda11.0.2_rocm5.4.3.tar.gz
